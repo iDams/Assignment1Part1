979000e69752756bb671c0ad91fd642071845161
Marco Astorga <marcoastorga.g@gmail.com>
2023-01-29 11:35:51 -0500

    fix validation

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -52,23 +52,51 @@ namespace Assignment1
                 if (isWeight)
                 {
                     Console.WriteLine("Insert weight");
+                    try
+                    {
+                        double value = double.Parse(Console.ReadLine());
+                        if (value <= 0)
+                        {
+                            Console.WriteLine("Invalid choice insert a positive number");
+                            continue;
+                        }
+                        else
+                        {
+                            return value;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Invalid choice insert a number");
+                        continue;
+                    }
                 }
                 else
                 {
                     Console.WriteLine("Number of bags");
+                    try
+                    {
+                        int value = int.Parse(Console.ReadLine());
+                        if (value <= 0)
+                        {
+                            Console.WriteLine("Invalid choice insert a positive number");
+                            continue;
+                        }
+                        else
+                        {
+                            return value;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Invalid choice insert a number");
+                        continue;
+                    }
+
                 }
 
 
-                try
-                {
-                    double value = double.Parse(Console.ReadLine());
-                    return value;
-                }
-                catch (Exception err)
-                {
-                    Console.WriteLine("Invalid choice " + err.Message);
-                    continue;
-                }
+
             }
 
         }
@@ -91,19 +119,21 @@ namespace Assignment1
             double totalApples = 0;
             double totalMilk = 0;
             double totalSceneCardPoint = 0;
-            double milkBag = 0;
             double storeBagsCost = 0;
-            int storeBagsAmount = 0;
+            double storeBagsValue = 0.50;
+            double hstTax = 0.13;
+
 
 
 
             String choice = "";
             bool keepRunning = true;
             MyChoices choices = new MyChoices();
-            Welcome();
+
 
             do
             {
+                Welcome();
                 Menu();
                 try
                 {
@@ -117,155 +147,177 @@ namespace Assignment1
                     continue;
                 }
 
-                if (choice == "done")
+                if (choice == "done" && totalChillies == 0 && totalTomatoes == 0 && totalApples == 0 && totalMilk == 0)
                 {
                     Console.Clear();
-                    Console.WriteLine("Do you have a scene card?");
-                    string sceneCard = Console.ReadLine();
-
-                    while (sceneCard != "yes" && sceneCard != "no")
-                    {
-                        Console.WriteLine("Invalid choice");
-                        Console.WriteLine("Do you have a scene card?");
-                        sceneCard = Console.ReadLine();
-                    }
-
-                    if (sceneCard == "yes")
-                    {
-                        subtotalChillies = 90 * (totalChillies* chillies) / 100;
-                        subtotalTomatoes = 90 * (totalTomatoes* tomatoes) / 100;
-                        subtotalApples = 90 * (totalApples* apples) / 100;
-                        subtotalMilk = totalMilk* (milk-0.54);
-                        totalSceneCardPoint = (totalChillies + totalTomatoes + totalApples) * 20;
-                        subtotal = subtotalChillies + subtotalTomatoes + subtotalApples + subtotalMilk;
-                    }
-                    else if (sceneCard == "no")
-                    {
-                        subtotalChillies += totalChillies * chillies;
-                        subtotalTomatoes += totalTomatoes * tomatoes;
-                        subtotalApples += totalApples * apples;
-                        subtotalMilk += totalMilk * milk;
-                        subtotal = subtotalChillies + subtotalTomatoes + subtotalApples + subtotalMilk;
-                    }
-
-                    Console.WriteLine("Do you want to buy bags?");
-                    string bags = Console.ReadLine();
-
-                    while (bags != "yes" && bags != "no")
-                    {
-                        Console.WriteLine("Invalid choice");
-                        Console.WriteLine("Do you want to buy bags?");
-                        bags = Console.ReadLine();
-                    }
-
-                    if (bags == "yes")
-                    {
-                        Console.WriteLine("How many bags do you want?");
-                        storeBagsAmount = int.Parse(Console.ReadLine());
-                        storeBagsCost = storeBagsAmount * 0.50;
-                        subtotal += storeBagsCost;
-                    }
-
-             
-                    Console.Clear();
-
-                    if (totalMilk > 0)
-                    {
-                        Console.WriteLine(MyChoices.MilkBags);
-                        Console.WriteLine("$" + subtotalMilk);
-                        Console.WriteLine("");
-                    }
-                    if (totalChillies > 0)
-                    {
-                        Console.WriteLine(MyChoices.Chillies);
-                        Console.WriteLine("$" + subtotalChillies);
-                        Console.WriteLine("");
-                    }  Console.WriteLine("");
-                    if (totalTomatoes > 0)
-                    {
-                        Console.WriteLine(MyChoices.Tomatoes);
-                        Console.WriteLine("$" + subtotalMilk);
-                        Console.WriteLine("");
-                    }
-                    if (totalApples > 0)
-                    {
-                        Console.WriteLine(MyChoices.Apples);
-                        Console.WriteLine("$" + subtotalApples);
-                        Console.WriteLine("");
-                    }
-                    Console.WriteLine("----------------");
-
+                    Console.WriteLine("!!! You have not selected any item !!!");
                     Console.WriteLine("");
-                    Console.WriteLine("Store Bags Cost $" + storeBagsCost);
-                    Console.WriteLine("");
-                    Console.WriteLine("Subtotal Price $" + subtotal);
-                    Console.WriteLine("HST Amount $" + subtotal * 0.13);
-                    Console.WriteLine("");
-                    Console.WriteLine("Grand Total $" + (subtotal * 0.13 + subtotal));
-                    Console.WriteLine("Total Scene Points Earnerd " + totalSceneCardPoint);
-                    keepRunning = false;
-                }
-
-
-                else
+                    keepRunning = true;
+                } else
                 {
-
-
-                    bool next = true;
-
-                    switch (choice.ToLower())
+                    if (choice == "done")
                     {
-                        case "chillies":
-                        case "chillie":
-                            choices = MyChoices.Chillies;
-                            break;
-                        case "tomatoes":
-                        case "tomatoe":
-                            choices = MyChoices.Tomatoes;
-                            break;
-                        case "apples":
-                        case "apple":
-                            choices = MyChoices.Apples;
-                            break;
-                        case "milkbags":
-                        case "milk":
-                            choices = MyChoices.MilkBags;
-                            break;
-                        default:
-                            next = false;
-                            keepRunning = true;
-                            Console.WriteLine("invalid choice");
-                            break;
-                    }
+                        Console.Clear();
+                        Console.WriteLine("Do you have a scene card? (yes/no)");
+                        string sceneCard = Console.ReadLine();
 
-
-                    if (next)
-                    {
-                        keepRunning = true;
-                        switch (choices)
+                        while (sceneCard != "yes" && sceneCard != "no")
                         {
-                            case MyChoices.Chillies:
-                                Console.WriteLine("You chose " + MyChoices.Chillies.ToString() + " " + chillies + "$ per lb");
-                                totalChillies += ValidateWeightOrBag(true);
+                            Console.WriteLine("Invalid choice");
+                            Console.WriteLine("Do you have a scene card? (yes/no)");
+                            sceneCard = Console.ReadLine();
+                        }
+
+                        if (sceneCard == "yes")
+                        {
+                            subtotalChillies = 90 * (totalChillies * chillies) / 100;
+                            subtotalTomatoes = 90 * (totalTomatoes * tomatoes) / 100;
+                            subtotalApples = 90 * (totalApples * apples) / 100;
+                            subtotalMilk = totalMilk * (milk - 0.54);
+                            totalSceneCardPoint = (totalChillies + totalTomatoes + totalApples) * 20;
+                            subtotal = subtotalChillies + subtotalTomatoes + subtotalApples + subtotalMilk;
+                        }
+                        else if (sceneCard == "no")
+                        {
+                            subtotalChillies += totalChillies * chillies;
+                            subtotalTomatoes += totalTomatoes * tomatoes;
+                            subtotalApples += totalApples * apples;
+                            subtotalMilk += totalMilk * milk;
+                            subtotal = subtotalChillies + subtotalTomatoes + subtotalApples + subtotalMilk;
+                        }
+
+                        Console.WriteLine("Do you want to buy bags? (yes/no)");
+                        string bags = Console.ReadLine();
+
+                        while (bags != "yes" && bags != "no")
+                        {
+                            Console.WriteLine("Invalid choice");
+                            Console.WriteLine("Do you want to buy bags? (yes/no)");
+                            bags = Console.ReadLine();
+                        }
+
+                        if (bags == "yes")
+                        {
+                            Console.WriteLine("How many bags do you want?");
+                            int storeBagsAmount = int.Parse(Console.ReadLine());
+                            storeBagsCost = storeBagsAmount * storeBagsValue;
+                            subtotal += storeBagsCost;
+                        }
+
+
+                        Console.Clear();
+
+                        if (totalMilk > 0)
+                        {
+                            Console.WriteLine(MyChoices.MilkBags);
+                            Console.WriteLine("$" + subtotalMilk);
+                            Console.WriteLine("");
+                        }
+                        if (totalChillies > 0)
+                        {
+                            Console.WriteLine(MyChoices.Chillies);
+                            Console.WriteLine("$" + subtotalChillies);
+                            Console.WriteLine("");
+                        }
+                        Console.WriteLine("");
+                        if (totalTomatoes > 0)
+                        {
+                            Console.WriteLine(MyChoices.Tomatoes);
+                            Console.WriteLine("$" + subtotalTomatoes);
+                            Console.WriteLine("");
+                        }
+                        if (totalApples > 0)
+                        {
+                            Console.WriteLine(MyChoices.Apples);
+                            Console.WriteLine("$" + subtotalApples);
+                            Console.WriteLine("");
+                        }
+                        Console.WriteLine("----------------");
+
+                        Console.WriteLine("");
+                        Console.WriteLine("Store Bags Cost $" + storeBagsCost);
+                        Console.WriteLine("");
+                        Console.WriteLine("Subtotal Price $" + subtotal);
+                        Console.WriteLine("HST Amount $" + (subtotal * hstTax).ToString("N2"));
+                        Console.WriteLine("");
+                        Console.WriteLine("Grand Total $" + (subtotal * hstTax + subtotal).ToString("N2"));
+                        Console.WriteLine("Total Scene Points Earnerd " + totalSceneCardPoint);
+                        keepRunning = false;
+                    }
+
+
+                    else
+                    {
+
+
+                        bool next = true;
+
+                        switch (choice.ToLower())
+                        {
+                            case "chillies":
+                            case "chillie":
+                                choices = MyChoices.Chillies;
                                 break;
-                            case MyChoices.Tomatoes:
-                                Console.WriteLine("You chose " + MyChoices.Tomatoes.ToString() + " " + tomatoes + "$ per lb");
-                                totalTomatoes += ValidateWeightOrBag(true);
+                            case "tomatoes":
+                            case "tomatoe":
+                                choices = MyChoices.Tomatoes;
                                 break;
-                            case MyChoices.Apples:
-                                Console.WriteLine("You chose " + MyChoices.Apples.ToString() + " " + apples + "$ per lb");
-                                totalApples += ValidateWeightOrBag(true);
+                            case "apples":
+                            case "apple":
+                                choices = MyChoices.Apples;
                                 break;
-                            case MyChoices.MilkBags:
-                                Console.WriteLine("You chose " + MyChoices.MilkBags.ToString() + " " + milk + "$ per bag");
-                                totalMilk += ValidateWeightOrBag(false);
+                            case "milkbags":
+                            case "milk":
+                                choices = MyChoices.MilkBags;
                                 break;
                             default:
+                                next = false;
+                                keepRunning = true;
+                                Console.Clear();
+                                Console.WriteLine("invalid choice");
                                 break;
                         }
-                    }
 
+
+                        if (next)
+                        {
+                            keepRunning = true;
+                            switch (choices)
+                            {
+                                case MyChoices.Chillies:
+                                    Console.WriteLine("You chose " + MyChoices.Chillies.ToString() + " " + chillies + "$ per lb");
+                                    totalChillies += ValidateWeightOrBag(true);
+                                    Console.Clear();
+                                    Console.WriteLine("You last chose is " + MyChoices.Chillies.ToString());
+                                    break;
+                                case MyChoices.Tomatoes:
+                                    Console.WriteLine("You chose " + MyChoices.Tomatoes.ToString() + " " + tomatoes + "$ per lb");
+                                    totalTomatoes += ValidateWeightOrBag(true);
+                                    Console.Clear();
+                                    Console.WriteLine("You last chose is " + MyChoices.Tomatoes.ToString());
+                                    break;
+                                case MyChoices.Apples:
+                                    Console.WriteLine("You chose " + MyChoices.Apples.ToString() + " " + apples + "$ per lb");
+                                    totalApples += ValidateWeightOrBag(true);
+                                    Console.Clear();
+                                    Console.WriteLine("You last chose is " + MyChoices.Apples.ToString());
+                                    break;
+                                case MyChoices.MilkBags:
+                                    Console.WriteLine("You chose " + MyChoices.MilkBags.ToString() + " " + milk + "$ per bag");
+                                    totalMilk += ValidateWeightOrBag(false);
+                                    Console.Clear();
+                                    Console.WriteLine("You last chose is " + MyChoices.MilkBags.ToString());
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+
+                    }
                 }
+
+
+
 
 
             } while (keepRunning);
